8723424a5b40c4e2ec977364e56be1b79b9e704a
takubii <vicsat93@gmail.com>
2024-05-25 18:35:43 +0900

    add: データ取得用のtanstack query hookを作成

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,15 +1,27 @@
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+import { eq } from 'drizzle-orm';
 import { Hono } from 'hono';
+import { HTTPException } from 'hono/http-exception';
 
 import { db } from '@/db/drizzle';
 import { accounts } from '@/db/schema';
 
-const app = new Hono().get('/', async (c) => {
+const app = new Hono().get('/', clerkMiddleware(), async (c) => {
+  const auth = getAuth(c);
+
+  if (!auth?.userId) {
+    throw new HTTPException(401, {
+      res: c.json({ error: 'Unauthorized' }, 401),
+    });
+  }
+
   const data = await db
     .select({
       id: accounts.id,
       name: accounts.name,
     })
-    .from(accounts);
+    .from(accounts)
+    .where(eq(accounts.userId, auth.userId));
 
   return c.json({ data });
 });
