b6bbb083f0c06f13da9f1f8d4a517576a71dd286
takubii <vicsat93@gmail.com>
2024-05-26 00:21:02 +0900

    add: account追加用のformを作成

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,3 +1,6 @@
+import { z } from 'zod';
+
+import { AccountForm } from '@/features/accounts/components/account-form';
 import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
 
 import {
@@ -7,9 +10,21 @@ import {
   SheetHeader,
   SheetTitle,
 } from '@/components/ui/sheet';
+import { insertAccountsSchema } from '@/db/schema';
+
+const formSchema = insertAccountsSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
 
 export const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount();
+
+  const onSubmit = (values: FormValues) => {
+    console.log({ values });
+  };
+
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
       <SheetContent className='space-y-4'>
@@ -17,6 +32,13 @@ export const NewAccountSheet = () => {
           <SheetTitle>New Account</SheetTitle>
           <SheetDescription>Create a new account to track your transactions.</SheetDescription>
         </SheetHeader>
+        <AccountForm
+          onSubmit={onSubmit}
+          defaultValues={{
+            name: '',
+          }}
+          disabled={false}
+        />
       </SheetContent>
     </Sheet>
   );
