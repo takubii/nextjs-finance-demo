455bab5397e7f1f33197d1b43c408072677ae07a
takubii <vicsat93@gmail.com>
2024-05-28 23:04:12 +0900

    add: sheetの削除機能を追加

--- features/accounts/components/edit-account-sheet.tsx
+++ features/accounts/components/edit-account-sheet.tsx
@@ -1,5 +1,7 @@
+import { Loader2 } from 'lucide-react';
 import { z } from 'zod';
 
+import { useDeleteAccount } from '@/features/accounts/api/use-delete-account';
 import { useEditAccount } from '@/features/accounts/api/use-edit-account';
 import { useGetAccount } from '@/features/accounts/api/use-get-account';
 import { AccountForm } from '@/features/accounts/components/account-form';
@@ -13,7 +15,7 @@ import {
   SheetTitle,
 } from '@/components/ui/sheet';
 import { insertAccountsSchema } from '@/db/schema';
-import { Loader2 } from 'lucide-react';
+import { useConfirm } from '@/hooks/use-confirm';
 
 const formSchema = insertAccountsSchema.pick({
   name: true,
@@ -24,10 +26,16 @@ type FormValues = z.input<typeof formSchema>;
 export const EditAccountSheet = () => {
   const { id, isOpen, onClose } = useOpenAccount();
 
+  const [ConfirmDialog, confirm] = useConfirm(
+    'Are you sure?',
+    'You are about to delete this transaction'
+  );
+
   const accountQuery = useGetAccount(id);
   const editMutation = useEditAccount(id);
+  const deleteMutation = useDeleteAccount(id);
 
-  const isPending = editMutation.isPending;
+  const isPending = editMutation.isPending || deleteMutation.isPending;
 
   const isLoading = accountQuery.isLoading;
 
@@ -39,6 +47,18 @@ export const EditAccountSheet = () => {
     });
   };
 
+  const onDelete = async () => {
+    const ok = await confirm();
+
+    if (ok) {
+      deleteMutation.mutate(undefined, {
+        onSuccess: () => {
+          onClose();
+        },
+      });
+    }
+  };
+
   const defaultValues = accountQuery.data
     ? {
         name: accountQuery.data.name,
@@ -48,25 +68,29 @@ export const EditAccountSheet = () => {
       };
 
   return (
-    <Sheet open={isOpen} onOpenChange={onClose}>
-      <SheetContent className='space-y-4'>
-        <SheetHeader>
-          <SheetTitle>Edit Account</SheetTitle>
-          <SheetDescription>Edit an existing account</SheetDescription>
-        </SheetHeader>
-        {isLoading ? (
-          <div className='absolute inset-0 flex items-center justify-center'>
-            <Loader2 className='size-4 text-muted-foreground animate-spin' />
-          </div>
-        ) : (
-          <AccountForm
-            id={id}
-            onSubmit={onSubmit}
-            defaultValues={defaultValues}
-            disabled={isPending}
-          />
-        )}
-      </SheetContent>
-    </Sheet>
+    <>
+      <ConfirmDialog />
+      <Sheet open={isOpen} onOpenChange={onClose}>
+        <SheetContent className='space-y-4'>
+          <SheetHeader>
+            <SheetTitle>Edit Account</SheetTitle>
+            <SheetDescription>Edit an existing account</SheetDescription>
+          </SheetHeader>
+          {isLoading ? (
+            <div className='absolute inset-0 flex items-center justify-center'>
+              <Loader2 className='size-4 text-muted-foreground animate-spin' />
+            </div>
+          ) : (
+            <AccountForm
+              id={id}
+              defaultValues={defaultValues}
+              onSubmit={onSubmit}
+              onDelete={onDelete}
+              disabled={isPending}
+            />
+          )}
+        </SheetContent>
+      </Sheet>
+    </>
   );
 };
