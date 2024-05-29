b5872b4013424c18874b4654d42d2ea89bead745
takubii <vicsat93@gmail.com>
2024-05-30 00:52:21 +0900

    add: categoryページのテーブルカラム設定追加

--- app/(dashboard)/categories/page.tsx
+++ app/(dashboard)/categories/page.tsx
@@ -11,7 +11,7 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
 
-import { columns } from '../accounts/columns';
+import { columns } from './columns';
 
 const CategoriesPage = () => {
   const categoriesQuery = useGetCategories();
