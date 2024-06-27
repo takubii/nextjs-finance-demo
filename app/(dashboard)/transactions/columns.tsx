cd0679b1818f61dba6cccd28d3a55a5c6f30ede9
takubii <vicsat93@gmail.com>
2024-06-27 21:50:23 +0900

    add: transactionsページのテーブルを作成

---
+++ app/(dashboard)/transactions/columns.tsx
@@ -0,0 +1,124 @@
+'use client';
+
+import { ColumnDef } from '@tanstack/react-table';
+import { format } from 'date-fns';
+import { InferResponseType } from 'hono';
+import { ArrowUpDown } from 'lucide-react';
+
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import { client } from '@/lib/hono';
+import { formatCurrency } from '@/lib/utils';
+
+export type ResponseType = InferResponseType<typeof client.api.transactions.$get, 200>['data'][0];
+
+export const columns: ColumnDef<ResponseType>[] = [
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && 'indeterminate')
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label='Select all'
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: 'date',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Date
+          <ArrowUpDown className='size-4 ml-2' />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const date = row.getValue('date') as Date;
+
+      return <span>{format(date, 'dd MMMM, yyyy')}</span>;
+    },
+  },
+  {
+    accessorKey: 'category',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Category
+          <ArrowUpDown className='size-4 ml-2' />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <span>{row.original.category}</span>;
+    },
+  },
+  {
+    accessorKey: 'payee',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Payee
+          <ArrowUpDown className='size-4 ml-2' />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: 'amount',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Amount
+          <ArrowUpDown className='size-4 ml-2' />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue('amount'));
+
+      return (
+        <Badge
+          variant={amount < 0 ? 'destructive' : 'primary'}
+          className='text-xs font-medium px-3.5 py-2.5'
+        >
+          {formatCurrency(amount)}
+        </Badge>
+      );
+    },
+  },
+  {
+    accessorKey: 'account',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Account
+          <ArrowUpDown className='size-4 ml-2' />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <span>{row.original.account}</span>;
+    },
+  },
+];
--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,14 +1,42 @@
 'use client';
 
-import { Plus } from 'lucide-react';
+import { Loader2, Plus } from 'lucide-react';
 
+import { useBulkDeleteTransactions } from '@/features/transactions/api/use-bulk-delete-transactions';
+import { useGetTransactions } from '@/features/transactions/api/use-get-transactions';
 import { useNewTransaction } from '@/features/transactions/hooks/use-new-transaction';
 
+import { DataTable } from '@/components/data-table';
 import { Button } from '@/components/ui/button';
-import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
+
+import { columns } from './columns';
 
 const TransactionsPage = () => {
   const newTransaction = useNewTransaction();
+  const deleteTransactions = useBulkDeleteTransactions();
+  const transactionsQuery = useGetTransactions();
+  const transactions = transactionsQuery.data || [];
+
+  const isDisabled = transactionsQuery.isLoading || deleteTransactions.isPending;
+
+  if (transactionsQuery.isLoading) {
+    return (
+      <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
+        <Card className='border-none drop-shadow-sm'>
+          <CardHeader>
+            <Skeleton className='w-48 h-8' />
+          </CardHeader>
+          <CardContent>
+            <div className='w-full h-[500px] flex items-center justify-center'>
+              <Loader2 className='size-6 text-slate-300 animate-spin' />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
 
   return (
     <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
@@ -20,6 +48,18 @@ const TransactionsPage = () => {
             <Plus className='size-4 mr-2' />
           </Button>
         </CardHeader>
+        <CardContent>
+          <DataTable
+            columns={columns}
+            data={transactions}
+            filterKey='name'
+            onDelete={(row) => {
+              const ids = row.map((r) => r.original.id);
+              deleteTransactions.mutate({ ids });
+            }}
+            disabled={isDisabled}
+          />
+        </CardContent>
       </Card>
     </div>
   );
