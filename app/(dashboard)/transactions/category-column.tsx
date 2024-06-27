423152abea45f38a5d001cfc9e6b570b5a55ad08
takubii <vicsat93@gmail.com>
2024-06-28 00:05:47 +0900

    add: カテゴリーが設定されていないときにクリックしたときにtransaction編集ページを開く処理を追加、検索が動作するように処理を追加

--- app/(dashboard)/transactions/category-column.tsx
+++ app/(dashboard)/transactions/category-column.tsx
@@ -2,6 +2,8 @@ import { TriangleAlert } from 'lucide-react';
 
 import { useOpenCategory } from '@/features/categories/hooks/use-open-category';
 
+import { useOpenTransaction } from '@/features/transactions/hooks/use-open-transaction';
+
 import { cn } from '@/lib/utils';
 
 type Props = {
@@ -12,10 +14,13 @@ type Props = {
 
 export const CategoryColumn = ({ id, category, categoryId }: Props) => {
   const { onOpen: onOpenCategory } = useOpenCategory();
+  const { onOpen: onOpenTransaction } = useOpenTransaction();
 
   const onClick = () => {
     if (categoryId) {
       onOpenCategory(categoryId);
+    } else {
+      onOpenTransaction(id);
     }
   };
 
