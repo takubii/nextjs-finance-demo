8abcabcbde466f509ed81cb7cf8dd8719cabdacb
takubii <vicsat93@gmail.com>
2024-07-02 23:48:39 +0900

    fix: 編集時にamountの値を変更する部分を修正

--- features/transactions/api/use-get-transaction.ts
+++ features/transactions/api/use-get-transaction.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { client } from '@/lib/hono';
+import { convertAmountFromMiliunits } from '@/lib/utils';
 
 export const useGetTransaction = (id?: string) => {
   const query = useQuery({
@@ -17,7 +18,10 @@ export const useGetTransaction = (id?: string) => {
 
       const { data } = await response.json();
 
-      return data;
+      return {
+        ...data,
+        amount: convertAmountFromMiliunits(data.amount),
+      };
     },
   });
 
