6049339b1e60609024f9825abc04e82ef12e2642
takubii <vicsat93@gmail.com>
2024-07-04 22:41:59 +0900

    add: DataCardコンポーネントを作成

--- components/data-grid.tsx
+++ components/data-grid.tsx
@@ -1,10 +1,13 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
+import { FaPiggyBank } from 'react-icons/fa';
 
 import { useGetSummary } from '@/features/summary/api/use-get-summary';
 
 import { formatDateRange } from '@/lib/utils';
+
+import { DataCard } from '@/components/data-card';
 
 export const DataGrid = () => {
   const { data } = useGetSummary();
@@ -15,5 +18,16 @@ export const DataGrid = () => {
 
   const dateRangeLabel = formatDateRange({ to, from });
 
-  return <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8'>{dateRangeLabel}</div>;
+  return (
+    <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8'>
+      <DataCard
+        title='Remaining'
+        value={data?.remainingAmount}
+        percentageChange={data?.remainingChange}
+        icon={FaPiggyBank}
+        variant='default'
+        dateRange={dateRangeLabel}
+      />
+    </div>
+  );
 };
