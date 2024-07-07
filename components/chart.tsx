0a2485ca6175e85f2b09f73288a2c507f9616074
takubii <vicsat93@gmail.com>
2024-07-07 18:48:26 +0900

    add: selectボックスからチャートの種類を変更できるように処理を追加

--- components/chart.tsx
+++ components/chart.tsx
@@ -1,9 +1,17 @@
-import { FileSearch } from 'lucide-react';
+import { AreaChart, BarChart3, FileSearch, LineChart } from 'lucide-react';
+import { useState } from 'react';
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
-// import { AreaVariant } from '@/components/area-variant';
-// import { BarVariant } from '@/components/bar-variant';
+import { AreaVariant } from '@/components/area-variant';
+import { BarVariant } from '@/components/bar-variant';
 import { LineVariant } from '@/components/line-variant';
 
 type Props = {
@@ -15,11 +23,43 @@ type Props = {
 };
 
 export const Chart = ({ data = [] }: Props) => {
+  const [chartType, setChartType] = useState('area');
+
+  const onTypeChange = (type: string) => {
+    // TODO: Add paywall
+
+    setChartType(type);
+  };
+
   return (
     <Card className='border-none drop-shadow-sm'>
       <CardHeader className='flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between'>
         <CardTitle className='text-xl line-clamp-1'>Transactions</CardTitle>
-        {/* TODO: Add Select */}
+        <Select defaultValue={chartType} onValueChange={onTypeChange}>
+          <SelectTrigger className='lg:w-auto h-9 rounded-md px-3'>
+            <SelectValue placeholder='Chart type' />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value='area'>
+              <div className='flex items-center'>
+                <AreaChart className='size-4 mr-2 shrink-0' />
+                <p className='line-clamp-1'>Area chart</p>
+              </div>
+            </SelectItem>
+            <SelectItem value='line'>
+              <div className='flex items-center'>
+                <LineChart className='size-4 mr-2 shrink-0' />
+                <p className='line-clamp-1'>Line chart</p>
+              </div>
+            </SelectItem>
+            <SelectItem value='bar'>
+              <div className='flex items-center'>
+                <BarChart3 className='size-4 mr-2 shrink-0' />
+                <p className='line-clamp-1'>Bar chart</p>
+              </div>
+            </SelectItem>
+          </SelectContent>
+        </Select>
       </CardHeader>
       <CardContent>
         {data.length === 0 ? (
@@ -28,9 +68,11 @@ export const Chart = ({ data = [] }: Props) => {
             <p className='text-muted-foreground text-sm'>No data for this period</p>
           </div>
         ) : (
-          // <AreaVariant data={data} />
-          // <BarVariant data={data} />
-          <LineVariant data={data} />
+          <>
+            {chartType === 'area' && <AreaVariant data={data} />}
+            {chartType === 'line' && <LineVariant data={data} />}
+            {chartType === 'bar' && <BarVariant data={data} />}
+          </>
         )}
       </CardContent>
     </Card>
