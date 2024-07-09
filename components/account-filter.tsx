2f151d06cc2a1543fc4fd02b60742e4fb85cdb1d
takubii <vicsat93@gmail.com>
2024-07-09 20:45:50 +0900

    add: AccountFilterにセレクトボックスを追加

--- components/account-filter.tsx
+++ components/account-filter.tsx
@@ -1,3 +1,31 @@
 'use client';
 
-export const AccountFilter = () => {};
+import { useGetAccounts } from '@/features/accounts/api/use-get-accounts';
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+export const AccountFilter = () => {
+  const { data: accounts, isLoading: isLoadingAccounts } = useGetAccounts();
+
+  return (
+    <Select value='' onValueChange={() => {}} disabled={false}>
+      <SelectTrigger className='lg:w-auto w-full h-9 rounded-md px-3 font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus:ring-offset-0 focus:ring-transparent outline-none text-white focus:bg-white/30 transition'>
+        <SelectValue placeholder='Account' />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value='all'>All accounts</SelectItem>
+        {accounts?.map((account) => (
+          <SelectItem key={account.id} value={account.id}>
+            {account.name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
