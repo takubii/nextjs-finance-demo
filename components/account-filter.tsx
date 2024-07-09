54fc5d768e5d9d78aa645c3e7f6d341870988f67
takubii <vicsat93@gmail.com>
2024-07-09 20:57:19 +0900

    add: AccountFilterにアカウント切り替え機能を追加

--- components/account-filter.tsx
+++ components/account-filter.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import qs from 'query-string';
+
 import { useGetAccounts } from '@/features/accounts/api/use-get-accounts';
 
 import {
@@ -11,12 +14,42 @@ import {
 } from '@/components/ui/select';
 
 export const AccountFilter = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const params = useSearchParams();
+  const accountId = params.get('accountId') || 'all';
+  const from = params.get('from') || '';
+  const to = params.get('to') || '';
+
   const { data: accounts, isLoading: isLoadingAccounts } = useGetAccounts();
 
+  const onChange = (newValue: string) => {
+    const query = {
+      accountId: newValue,
+      from,
+      to,
+    };
+
+    if (newValue === 'all') {
+      query.accountId = '';
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query,
+      },
+      { skipNull: true, skipEmptyString: true }
+    );
+
+    router.push(url);
+  };
+
   return (
-    <Select value='' onValueChange={() => {}} disabled={false}>
+    <Select value={accountId} onValueChange={onChange} disabled={isLoadingAccounts}>
       <SelectTrigger className='lg:w-auto w-full h-9 rounded-md px-3 font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus:ring-offset-0 focus:ring-transparent outline-none text-white focus:bg-white/30 transition'>
-        <SelectValue placeholder='Account' />
+        <SelectValue placeholder='Select account' />
       </SelectTrigger>
       <SelectContent>
         <SelectItem value='all'>All accounts</SelectItem>
