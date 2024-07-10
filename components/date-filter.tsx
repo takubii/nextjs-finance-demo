87ed7451a0608096e471d9d1e9519b22de327616
takubii <vicsat93@gmail.com>
2024-07-10 23:56:22 +0900

    add: DateFilterにカレンダーで範囲を決める処理を追加

--- components/date-filter.tsx
+++ components/date-filter.tsx
@@ -1,11 +1,15 @@
 'use client';
 
-import { subDays } from 'date-fns';
+import { format, subDays } from 'date-fns';
+import { ChevronDown } from 'lucide-react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-// import qs from 'query-string';
+import qs from 'query-string';
+import { useState } from 'react';
+import { DateRange } from 'react-day-picker';
 
 import { Button } from '@/components/ui/button';
-import { Popover, PopoverTrigger } from '@/components/ui/popover';
+import { Calendar } from '@/components/ui/calendar';
+import { Popover, PopoverClose, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { formatDateRange } from '@/lib/utils';
 
 export const DateFilter = () => {
@@ -13,7 +17,7 @@ export const DateFilter = () => {
   const pathname = usePathname();
 
   const params = useSearchParams();
-  const accountId = params.get('accountId') || 'all';
+  const accountId = params.get('accountId');
   const from = params.get('from') || '';
   const to = params.get('to') || '';
 
@@ -23,6 +27,31 @@ export const DateFilter = () => {
   const paramState = {
     from: from ? new Date(from) : defaultFrom,
     to: to ? new Date(to) : defaultTo,
+  };
+
+  const [date, setDate] = useState<DateRange | undefined>(paramState);
+
+  const pushToUrl = (dateRange: DateRange | undefined) => {
+    const query = {
+      accountId,
+      from: format(dateRange?.from || defaultFrom, 'yyyy-MM-dd'),
+      to: format(dateRange?.to || defaultTo, 'yyyy-MM-dd'),
+    };
+
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query,
+      },
+      { skipNull: true, skipEmptyString: true }
+    );
+
+    router.push(url);
+  };
+
+  const onReset = () => {
+    setDate(undefined);
+    pushToUrl(undefined);
   };
 
   return (
@@ -35,8 +64,41 @@ export const DateFilter = () => {
           className='lg:w-auto w-full h-9 rounded-md px-3 font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus:ring-offset-0 focus:ring-transparent outline-none text-white focus:bg-white/30 transition'
         >
           <span>{formatDateRange(paramState)}</span>
+          <ChevronDown className='ml-2 size-4 opacity-50' />
         </Button>
       </PopoverTrigger>
+      <PopoverContent className='lg:w-auto w-full p-0' align='start'>
+        <Calendar
+          disabled={false}
+          initialFocus
+          mode='range'
+          defaultMonth={date?.from}
+          selected={date}
+          onSelect={setDate}
+          numberOfMonths={2}
+        />
+        <div className='p-4 w-full flex items-center gap-x-2'>
+          <PopoverClose asChild>
+            <Button
+              onClick={onReset}
+              disabled={!date?.from || !date?.to}
+              className='w-full'
+              variant='outline'
+            >
+              Reset
+            </Button>
+          </PopoverClose>
+          <PopoverClose asChild>
+            <Button
+              onClick={() => pushToUrl(date)}
+              disabled={!date?.from || !date?.to}
+              className='w-full'
+            >
+              Apply
+            </Button>
+          </PopoverClose>
+        </div>
+      </PopoverContent>
     </Popover>
   );
 };
